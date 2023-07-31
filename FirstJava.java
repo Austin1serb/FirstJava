79b2235607f65987deb4d40c8f040099886d2ed4
Austin Serb <128577470+Austin1serb@users.noreply.github.com>
2023-07-31 14:46:09 -0700

    Add files via upload

---
+++ FirstJava.java
@@ -0,0 +1,8 @@
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("My Name is Coding Dojo\n");
+        System.out.println("I am 100 years old\n");
+        System.out.println("My hometown is Burbank, CA\n");
+    }
+}
+
